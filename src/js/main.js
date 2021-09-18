8c63615e86f540398d873895e981e90eea555cb8
FireBallDevelopment <inovomiast@hotmail.com>
2021-09-18 21:08:30 +0200

    Var

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,11 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const redir_from = urlParams.get('redirect_from');
+const email = urlParams.get('email');
 
-if(redir_from === 'gmail'){
-    console.log('Redirected From Gmail')
+
+const mails = ["juanfrancisco.medina@salesianos.edu", "joseantonio.perez@salesianos.edu", ""]
+
+if(redir_from === 'gmail' && email === mails[0]){
+    console.log(`Redirected From Gmail || Clicked by: ${mails[0]}`)
 }
