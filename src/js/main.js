a069a4e0e0793a38b5922da47e4488430f0ecfa7
FireBallDevelopment <inovomiast@hotmail.com>
2021-09-19 12:57:19 +0200

    Like system... fixed... I think so

--- src/js/main.js
+++ src/js/main.js
@@ -35,7 +35,9 @@ const like_btn = document.getElementById('like_btn');
 const like_btn_2 = document.getElementById('like_btn_2');
 const like_btn_3 = document.getElementById('like_btn_3');
 
-let liked = false
+let liked = false;
+let liked_2 = false;
+let liked_3 = false;
 
 like_btn.addEventListener('click', () => {
     if(liked === false) {
@@ -51,24 +53,24 @@ like_btn.addEventListener('click', () => {
 
 like_btn_2.addEventListener('click', () => {
     if(liked === false) {
-        like_btn.style.animation = 'like_roll_anim 0.7s ease-in';
-        like_btn.style.color = 'royalblue';
-        liked = true;
+        like_btn_2.style.animation = 'like_roll_anim 0.7s ease-in';
+        like_btn_2.style.color = 'royalblue';
+        liked_2 = true;
     }else{
-        like_btn.style.animation = 'unlike_roll_anim 0.7s ease-in';
-        like_btn.style.color = 'black';
-        liked = false;
+        like_btn_2.style.animation = 'unlike_roll_anim 0.7s ease-in';
+        like_btn_2.style.color = 'black';
+        liked_2 = false;
     }
 });
 
 like_btn_3.addEventListener('click', () => {
     if(liked === false) {
-        like_btn.style.animation = 'like_roll_anim 0.7s ease-in';
-        like_btn.style.color = 'royalblue';
-        liked = true;
+        like_btn_3.style.animation = 'like_roll_anim 0.7s ease-in';
+        like_btn_3.style.color = 'royalblue';
+        liked_3 = true;
     }else{
-        like_btn.style.animation = 'unlike_roll_anim 0.7s ease-in';
-        like_btn.style.color = 'black';
-        liked = false;
+        like_btn_3.style.animation = 'unlike_roll_anim 0.7s ease-in';
+        like_btn_3.style.color = 'black';
+        liked_3 = false;
     }
 });
