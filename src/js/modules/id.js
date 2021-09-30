9ff4e3b1763b0e08a19bb02b3dd740f52b754d57
FireBallDevelopment <inovomiast@hotmail.com>
2021-09-30 19:19:14 +0200

    ID Gen

---
+++ src/js/modules/id.js
@@ -0,0 +1,7 @@
+function generate(cq){
+    var fP = (Math.random() * 50000) | 0;
+    var sP = (Math.random() * 50000) | 0;
+    fP = ('000' + fP.toString(36)).slice(-3);
+    sP = ('000' + sP.toString(36)).slice(-3);
+    console.log(fP + sP);
+}
