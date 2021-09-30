5902bef3177f7d10431343015521a3ed569f82ca
FireBallDevelopment <inovomiast@hotmail.com>
2021-09-30 19:39:49 +0200

    Now YES1

--- src/js/modules/id.js
+++ src/js/modules/id.js
@@ -1,8 +1,8 @@
 function generate(){
-    var fP = (Math.random() * 45000) | 0;
-    var sP = (Math.random() * 45000) | 0;
-    fP = ('000' + fP.toString(36)).slice(-5);
-    sP = ('000' + sP.toString(36)).slice(-5);
+    var fP = (Math.random() * 50000) | 0;
+    var sP = (Math.random() * 50000) | 0;
+    fP = ('000' + fP.toString(36)).slice(-8);
+    sP = ('000' + sP.toString(36)).slice(-8);
     const result = fP + sP;
     const id_display = document.getElementById('id_display');
     id_display.innerText = result;
