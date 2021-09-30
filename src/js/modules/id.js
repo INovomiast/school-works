1a00f29a326ae9bb80605c57f52e59a971e2f297
FireBallDevelopment <inovomiast@hotmail.com>
2021-09-30 19:25:02 +0200

    Update id.js

--- src/js/modules/id.js
+++ src/js/modules/id.js
@@ -1,9 +1,12 @@
-function generate(cq){
+function generate(){
     var fP = (Math.random() * 50000) | 0;
     var sP = (Math.random() * 50000) | 0;
     fP = ('000' + fP.toString(36)).slice(-3);
     sP = ('000' + sP.toString(36)).slice(-3);
-    console.log(fP + sP);
+    const result = fP + sP;
+    const id_display = document.getElementById('');
+
+    id_display.innerText = result;
 }
 
 generate();
