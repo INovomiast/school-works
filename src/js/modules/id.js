76630af9a6d9e83e074df05539de4d103e6dde1a
FireBallDevelopment <inovomiast@hotmail.com>
2021-09-30 19:41:19 +0200

    More Changes for a new ID

--- src/js/modules/id.js
+++ src/js/modules/id.js
@@ -1,8 +1,8 @@
 function generate(){
     var fP = (Math.random() * 50000) | 0;
     var sP = (Math.random() * 50000) | 0;
-    fP = ('000' + fP.toString(36)).slice(-8);
-    sP = ('000' + sP.toString(36)).slice(-8);
+    fP = ('1fx2' + fP.toString(36)).slice(-8);
+    sP = ('1xz4' + sP.toString(36)).slice(-8);
     const result = fP + sP;
     const id_display = document.getElementById('id_display');
     id_display.innerText = result;
