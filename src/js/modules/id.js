e8c430890c058e9e1bf571cfebaead4824c4c511
FireBallDevelopment <inovomiast@hotmail.com>
2021-09-30 19:37:58 +0200

    Changes

--- src/js/modules/id.js
+++ src/js/modules/id.js
@@ -1,8 +1,8 @@
 function generate(){
-    var fP = (Math.random() * 60000) | 0;
-    var sP = (Math.random() * 60000) | 0;
-    fP = ('000' + fP.toString(36)).slice(-2);
-    sP = ('000' + sP.toString(36)).slice(-2);
+    var fP = (Math.random() * 45000) | 0;
+    var sP = (Math.random() * 45000) | 0;
+    fP = ('000' + fP.toString(36)).slice(-5);
+    sP = ('000' + sP.toString(36)).slice(-5);
     const result = fP + sP;
     const id_display = document.getElementById('id_display');
     id_display.innerText = result;
