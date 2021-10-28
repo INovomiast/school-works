cd8d6c5d6711e3c852f1819fb7207f6db17e714c
FireBallDevelopment <inovomiast@hotmail.com>
2021-10-28 19:26:15 +0200

    DEV MODE
    Cositas

---
+++ src/js/dev.js
@@ -0,0 +1,46 @@
+/**
+ * DEV.JS || A full Script for Creating Dev Tools
+ * Made By: INovomiast
+ * 
+ * Learn more on https://DEV.JS.glitch.me/?redir=script
+ */
+
+const devKeys = ['inovo16022005accMain'];
+
+//The thing that you wan't to be showed at the DevKey is Successful!
+const dev_tools = document.getElementById('dev-tools');
+const sep = document.getElementById('separator')
+
+//Key Combination for Dev Tools Accesing
+document.body.addEventListener('keydown', (event) => {
+    if(event.ctrlKey && event.key === 'ñ'){
+        const dev_access = prompt('Insert your DevCode before the tag devKey:', 'devKey:');
+        if(dev_access === "devKey:" + devKeys[0]){
+            alert('Access was succesful!');
+            sep.style.display = 'block';
+            dev_tools.style.display = 'block';
+            dev_tools.style.animation = 'bounceInRight';
+            dev_tools.style.animationDuration = '3s';
+            
+        }else{
+            alert('Unexistant DevKey!');
+        }
+    }
+})
+
+//My work has a link genrator, with pre-made links, it's just a "Share Link Creator"
+const share_link_tool = document.getElementById('');
+const generate_link_btn = document.getElementById('');
+const link_display = document.getElementById('');
+
+const links = ['', '', ''];
+
+generate_link_btn.addEventListener('click', () => {
+   if (window.location.href === ""){
+       
+   }else if(window.location.href === ""){
+       
+   }else if(window.location.href === ""){
+       
+   }
+});
